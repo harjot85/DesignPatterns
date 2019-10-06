c4b4fbc4aa0d06e57513344e13eef32aad2ee7ef
Harjot <singh.harjot85@gmail.com>
2019-10-06 11:34:44 -0700

    First commit - Singleton Pattern

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singletonEmp = Singleton.GetInstance;
+            singletonEmp.PrintDetails("This is the first message for employees...");
+
+            Singleton singletonContract = Singleton.GetInstance;
+            singletonContract.PrintDetails("Sending a message for Contractors ...");
+
+            Singleton.DerivedSingleton derived = new Singleton.DerivedSingleton();
+            derived.PrintDetails("Is this a hack? ");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    
+    class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                //Create an object only the first time 
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+        //A private constructor ensures object construction happens inside the class only
+        private Singleton()
+        {
+            Console.WriteLine(++counter);
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public class DerivedSingleton : Singleton
+        {
+
+        }
+    }
+}
